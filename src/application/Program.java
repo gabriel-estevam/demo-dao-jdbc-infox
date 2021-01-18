31036075dde7feb1397d645eae19ad4f449f5073
Gabriel Estevam Maciel <gabrielestevammaciel1@gmail.com>
2021-01-17 21:08:35 -0300

    Employee entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Employee;
 
 public class Program 
 {
 	public static void main(String[] args) 
 	{
-		Department obj = new Department(1, "Book");
-		System.out.println(obj);
+		Department obj = new Department(1,"Book");
+		Employee employee = new Employee(1, "Bob", new Date(), "Bob@gmail.com", 3000.0,obj);
+		
+		System.out.println(employee);
 	}
 }
---
+++ src/model/entities/Employee.java
@@ -0,0 +1,110 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Employee implements Serializable
+{
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String name;
+	private Date BirthDate;
+	private String Email;
+	private Double BaseSalary;
+	
+	private Department departmentId;
+	
+	public Employee() {
+		
+	}
+
+	public Employee(Integer id, String name, Date birthDate, String email, Double baseSalary, Department departmentId) 
+	{
+		this.id = id;
+		this.name = name;
+		BirthDate = birthDate;
+		Email = email;
+		BaseSalary = baseSalary;
+		this.departmentId = departmentId;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getBirthDate() {
+		return BirthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		BirthDate = birthDate;
+	}
+
+	public String getEmail() {
+		return Email;
+	}
+
+	public void setEmail(String email) {
+		Email = email;
+	}
+
+	public Double getBaseSalary() {
+		return BaseSalary;
+	}
+
+	public void setBaseSalary(Double baseSalary) {
+		BaseSalary = baseSalary;
+	}
+
+	public Department getDepartmentId() {
+		return departmentId;
+	}
+
+	public void setDepartmentId(Department departmentId) {
+		this.departmentId = departmentId;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", BirthDate=" + BirthDate + ", Email=" + Email
+				+ ", BaseSalary=" + BaseSalary + ", departmentId=" + departmentId + "]";
+	}
+}
